c7ecb811cd02eb9e5abc12953c08f73c4164e56b
Nelson Chen <nelsonchen1020@gmail.com>
2020-11-03 12:39:55 -0800

    add logic to swap alternate X to O

--- src/index.js
+++ src/index.js
@@ -2,18 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-
-    render() {
-        return (
-        <button 
-            className="square" 
-            onClick={ () => this.props.onClick() }
-        >
-            {this.props.value}
-        </button>
-        );
-    }
+function Square(props) {
+    return (
+    <button 
+        className="square" 
+        onClick={props.onClick }
+    >
+        {props.value}
+    </button>
+    );
 }
   
 class Board extends React.Component {
@@ -21,15 +18,19 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
-    
+
     renderSquare(i) {
         return (<Square 
             value={this.state.squares[i]} 
