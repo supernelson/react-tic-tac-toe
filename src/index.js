09cc676ec715199f0310f207d866479a25cf57b7
Nelson Chen <nelsonchen1020@gmail.com>
2020-11-04 11:07:55 -0800

    lifting state from Board to Game to accommodate history.

--- src/index.js
+++ src/index.js
@@ -14,13 +14,6 @@ function Square(props) {
 }
   
 class Board extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true,
-        };
-    }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
@@ -33,8 +26,8 @@ class Board extends React.Component {
 
     renderSquare(i) {
         return (<Square 
-            value={this.state.squares[i]} 
-            onClick = { () => this.handleClick(i) }
+            value={this.props.squares[i]} 
+            onClick = { () => this.props.onClick(i) }
             />
         );
     }
@@ -72,6 +65,16 @@ class Board extends React.Component {
   }
   
 class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            xIsNext: true,
+        };
+    }
+
     render() {
         return (
         <div className="game">
@@ -105,7 +108,7 @@ function calculateWinner(squares) {
       }
     }
     return null;
-  }
+}
   // ========================================
   
   ReactDOM.render(
