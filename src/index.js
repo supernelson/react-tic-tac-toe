adf44c99f3cbf3ad9d9d7ae92e6945c50fb2c305
Nelson Chen <nelsonchen1020@gmail.com>
2020-11-04 12:10:17 -0800

    rendering successful. showing past moves.

--- src/index.js
+++ src/index.js
@@ -27,7 +27,6 @@ class Board extends React.Component {
 
         return (
         <div>
-        <div className="status">{status}</div>
         <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
@@ -77,7 +76,21 @@ class Game extends React.Component {
     }
 
     render() {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const winner = calculateWinner(current.squares);
+        let status;
         
+        const moves = history.map((step, move) => {
+            const desc = move ?
+                'Go to move #' + move : 
+                'Go to game start';
+            return (
+                <li>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
 
         return (
         <div className="game">
@@ -89,7 +102,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
         </div>
         </div>
         );
